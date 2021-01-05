4b6ab659ccfb6af303d9cbb22dfd8829d91df73a
wlytle <wlytle89@gmail.com>
2021-01-05 10:26:35 -0700

    builds controlled edit form

--- app/javascript/components/EditPlayer.js
+++ app/javascript/components/EditPlayer.js
@@ -22,40 +22,70 @@ const handleSubmit = (e) => {
     .catch((err) => console.log(err));
 };
 
-const EditPlayer = (props) => {
+const EditPlayer = ({ user }) => {
   const [showEdit, setShowEdit] = useState(false);
-  const [userName, setUserName] = useState("");
+  const [userName, setUserName] = useState(user);
   const [password, setPassword] = useState("");
+  const [newPasswordConfirmation, setNewPasswordConfirmation] = useState("");
+  const [newPassword, setNewPassword] = useState("");
 
   return (
     <div>
       <div className="field">
         <div className="control">
           <input
-            className="input"
+            className="input is-danger"
             type="text"
             placeholder="Username"
             value={userName}
             onChange={(e) => setUserName(e.target.value)}
           />
+        </div>
+      </div>
+      <div className="field">
+        <div className="control">
+          <input
+            className="input is-success"
+            type="password"
+            name="new_password"
+            value={newPassword}
+            placeholder="New Password"
+            onChange={({ target }) => setNewPassword(target.value)}
+          />
+        </div>
+      </div>
+      <div className="field">
+        <div className="control">
           <input
             className="input"
-            type="text"
+            type="password"
+            name="new_password_conf"
+            value={newPasswordConfirmation}
+            placeholder="Confrim New Password"
+            onChange={({ target }) => setNewPasswordConfirmation(target.value)}
+          />
+        </div>
+      </div>
+      <div className="field">
+        <div className="control">
+          <input
+            className="input"
+            type="password"
             name="password"
             value={password}
             placeholder="Password"
             onChange={({ target }) => setPassword(target.value)}
           />
+        </div>
+      </div>
+      <div className="field">
+        <div className="control">
           <div className="buttons ">
             <button className="button">Update</button>
-            {/* should re-render same player info page with new username displayed */}
-
-            {/* enables new game button */}
           </div>
 
           <div className="mt-6 is-centered ">
             <button className="button is-danger">Delete Account</button>
-            {/* should redirect user to login-screen */}
           </div>
         </div>
       </div>
