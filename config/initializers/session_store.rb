2322d5a0894e10f2922eeaf1b3344c3b83d33813
Clayton Pierce <46853031+technicholy@users.noreply.github.com>
2021-02-10 16:58:28 -0800

    more production

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: '_onitama', domain: 'onitama.baracus.rocks'
+  Rails.application.config.session_store :cookie_store, key: '_onitama', domain: 'onitama.claytonpierce.dev'
 else
   Rails.application.config.session_store :cookie_store, key: '_onitama'
 end
